36a15c03729131ead30a9b11a66c57c60361ec63
solomon <solomon.chukwuka28@gmail.com>
2023-10-09 22:25:38 +0100

    create the controller and models

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,9 +1,85 @@
 import { NextRequest, NextResponse } from "next/server";
+import Room from "../models/room";
 
-const allRooms = async () => {
+export const allRooms = async (req: NextRequest) => {
+  const resPerPage: number = 8;
+  const rooms = await Room.find();
   return NextResponse.json({
-    data: "hello",
+    success: true,
+    resPerPage,
+    rooms,
   });
 };
 
-export default allRooms;
+// Create new room => /api/rooms
+export const newRoom = async (req: NextRequest) => {
+  const body = await req.json();
+  const rooms = await Room.create(body);
+
+  return NextResponse.json({
+    success: true,
+    rooms,
+  });
+};
+
+// Get rooms details => /api/rooms/:id
+export const getRoomDetails = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+// Update room details => /api/admin/rooms/id
+export const updateRoom = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  let room = await Room.findById(params.id);
+  const body = await req.json();
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      { status: 404 }
+    );
+  }
+  room = await Room.findByIdAndUpdate(params.id, body, {
+    new: true,
+  });
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+// Delete room detail => /api/admin/rooms/:id
+export const DeleteRoom = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+  if (!room) {
+    return NextResponse.json({ message: "Room not found" }, { status: 404 });
+  }
+  // TODO - Delete images associated with the room
+
+  await room.deleteOne();
+
+  return NextResponse.json({ success: true });
+};
