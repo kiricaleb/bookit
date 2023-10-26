8b49ae26008fe1e4f7e0ee2e7ba9907b4237e4e0
solomon <solomon.chukwuka28@gmail.com>
2023-10-26 14:13:48 +0100

    intergrade redux and register user

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -2,12 +2,16 @@
 
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
 
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Toaster />
-      <SessionProvider>{children}</SessionProvider>
+      <Provider store={store}>
+        <SessionProvider>{children}</SessionProvider>
+      </Provider>
     </>
   );
 }
