ccec0552365e8df44c9c7d0521fc5c633abafdd3
Solomon Chukwuka Emmanuel <solomon.chukwuka28@gmail.com>
2024-04-05 09:51:06 +0100

    lo

--- types/next.d.ts
+++ types/next.d.ts
@@ -1,10 +1,3 @@
-// import { IUser } from "@/backend/models/user";
-
-// declare module "next/server" {
-//   interface NextRequest {
-//     user: IUser;
-//   }
-// }
 import { IUser } from "@/backend/models/user";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
 import { NextRequest } from "next/server";
